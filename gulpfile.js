5f7cd9faa79a0a3d80880debd0126b0d6713f894
SerhiiTimchak <2freemangordon3@gmail.com>
2019-03-18 15:19:03 +0200

    setup build task that assembles project for production

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs');
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    del = require('del');
 
 
 // simple task 
@@ -59,10 +62,35 @@ gulp.task('browser-sync', function() {
     });
 });
 
+//This task need for minification css
+gulp.task('css-libs', function() {
+    return gulp.src('app/sass/_libs.scss')
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('app/css'));
+});
+
+// We should delete dir dist before build new production proj 
+gulp.task('clean', async function() {
+    return del.sync('dist');
+});
+// Buldig project for prodaction:
+gulp.task('prebuild', async function() {
+    var buildCss = gulp.src(['app/css/main.css', 'app/css/_libs.min.scss'])
+        .pipe(gulp.dest('dist/css'));
+
+    var buildJs = gulp.src(['app/js/**/*'])
+        .pipe(gulp.dest('dist/js'));
+    var buildHtml = gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
 // Separate task for wathcing by files 
 gulp.task('watch', function() {
     gulp.watch('app/sass/**/*.scss', gulp.parallel('sass')); // watching by files .scss
     gulp.watch('app/*.html', gulp.parallel('code'));
     gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('script'));
 });
-gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
+gulp.task('default', gulp.parallel('css-libs', 'sass', 'zipLibs', 'browser-sync', 'watch'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'sass', 'zipLibs'));
