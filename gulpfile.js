49cf6485b96df83e35be2db28cd9c25bbb3b3b9c
Serhii Tymchak <2freemangordon3@gmail.com>
2019-03-29 22:29:10 +0200

    add to the builder a font carring and zip&cache images

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,34 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
+    del = require('del'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglifyjs'),
-    cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename'),
-    del = require('del');
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache');
 
 
-// simple task 
-// gulp.task('mytask', async function() {
-//     console.log('Hello I`m Task');
-// });
 
-// This is a base of gulp.  
-gulp.task('mytask', function() {
-    return gulp.src('sorse-files').pipe(plugin()).pipe(gulp.dest('folder'));
-});
-
-// a simple example 
-// gulp.task('sass', function() {
-//     return gulp.src('app/sass/main.scss').pipe(sass()).pipe(gulp.dest('app/css'));
-// });
-
+//Task for Saas
 gulp.task('sass', function() {
     return gulp.src('app/sass/**/*.scss') // take the sourse 
-        .pipe(sass()) // covert scss to css 
+        .pipe(sass()) // convert scss to css 
         .pipe(gulp.dest('app/css')) // upload results into  app/css
         .pipe(browserSync.reload({ stream: true })); // refresh css on the page
 });
 
 //Task for zipping libs JS
 gulp.task('zipLibs', function() {
-    return gulp.src([
-            'app/libs/jquery/dist/jquery.min.js',
-            'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
-        ]).pipe(concat('libs.min.js'))
+    return gulp.src('app/libs/jquery/jquery.min.js')
+        .pipe(concat('libs.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('app/js'));
 });
 
 //Task for JS
 gulp.task('script', function() {
-    return gulp.src(['app/js/common.js', 'app/libs/**/*.js'])
+    return gulp.src(['app/js/common.js'])
         .pipe(browserSync.reload({ stream: true }));
 });
 
@@ -62,35 +48,41 @@ gulp.task('browser-sync', function() {
     });
 });
 
-//This task need for minification css
-gulp.task('css-libs', function() {
-    return gulp.src('app/sass/_libs.scss')
-        .pipe(cssnano())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('app/css'));
-});
-
 // We should delete dir dist before build new production proj 
 gulp.task('clean', async function() {
     return del.sync('dist');
 });
+
+
+gulp.task('img', function() {
+    return gulp.src("app/img/**/*")
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()],
+        })))
+        .pipe(gulp.dest('dist/img'));
+});
+
 // Buldig project for prodaction:
 gulp.task('prebuild', async function() {
-    var buildCss = gulp.src(['app/css/main.css', 'app/css/_libs.min.scss'])
+    var buildCss = gulp.src(['app/css/main.css'])
         .pipe(gulp.dest('dist/css'));
-
     var buildJs = gulp.src(['app/js/**/*'])
         .pipe(gulp.dest('dist/js'));
     var buildHtml = gulp.src('app/*.html')
         .pipe(gulp.dest('dist'));
+    var fonts = gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
 });
 
 // Separate task for wathcing by files 
 gulp.task('watch', function() {
     gulp.watch('app/sass/**/*.scss', gulp.parallel('sass')); // watching by files .scss
     gulp.watch('app/*.html', gulp.parallel('code'));
-    gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('script'));
+    gulp.watch(['app/js/common.js'], gulp.parallel('script'));
 });
 
-gulp.task('default', gulp.parallel('css-libs', 'sass', 'zipLibs', 'browser-sync', 'watch'));
-gulp.task('build', gulp.parallel('prebuild', 'clean', 'sass', 'zipLibs'));
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch', 'zipLibs'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'sass', 'zipLibs', 'img'));
